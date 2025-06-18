84c0fbd12168e4fefb3e36734e8e6eae4e0a35a9
imabhisht <abhishtchouhan@gmail.com>
2025-06-18 13:40:22 +0530

    feat: remove unused Instructions import from Chat component

--- web/src/components/chat.tsx
+++ web/src/components/chat.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { Instructions } from "@/components/instructions";
 import { SessionControls } from "@/components/session-controls";
 import { ConnectButton } from "./connect-button";
 import { ConnectionState } from "livekit-client";
